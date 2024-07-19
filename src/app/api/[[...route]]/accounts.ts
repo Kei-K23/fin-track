c2cf468ebe0a58bb0cd7f990fcdc37ad3003cd11
Kei-K23 <keiksl2301@gmail.com>
2024-07-19 09:59:34 +0630

    Add bulk delete post method

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -2,9 +2,10 @@ import { db } from "@/db/drizzle";
 import { accounts, insertAccountSchema } from "@/db/schema";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator"
 import { createId } from "@paralleldrive/cuid2"
+import { z } from "zod";
 
 const app = new Hono()
     .get("/", clerkMiddleware(), async (c) => {
@@ -42,6 +43,30 @@ const app = new Hono()
         }).returning();
 
         return c.json({ data });
-    });
+    })
+    .post("/bulk-delete", clerkMiddleware(), zValidator("json", z.object({
+        ids: z.array(z.string().min(1))
+    })), async (c) => {
+        const auth = getAuth(c);
+        const values = c.req.valid("json");
+        // If user is not authenticated
+        if (!auth?.userId) {
+            return c.json({
+                error: "Unauthorize user"
+            }, 401);
+        }
+
+        const data = await db.delete(accounts).where(
+            and(
+                eq(accounts.userId, auth.userId),
+                inArray(accounts.id, values.ids)
+            )
+        ).returning({
+            id: accounts.id,
+        });
+
+        return c.json({ data });
+    })
+    ;
 
 export default app;
