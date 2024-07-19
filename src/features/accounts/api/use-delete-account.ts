a8c86c6eb5c202b0e9d21607d1d2e29ff69c6cc7
Kei-K23 <keiksl2301@gmail.com>
2024-07-19 17:52:04 +0630

    Add bulk delete post method

---
+++ src/features/accounts/api/use-delete-account.ts
@@ -0,0 +1,27 @@
+import { InferRequestType, InferResponseType } from "hono"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { client } from "@/lib/hono"
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.accounts["bulk-delete"]["$post"]>;
+type RequestType = InferRequestType<typeof client.api.accounts["bulk-delete"]>["json"];
+
+export const useCreateAccount = () => {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await client.api.accounts["bulk-delete"]["$post"]({ json });
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Accounts deleted successfully");
+            queryClient.invalidateQueries({ queryKey: ["accounts"] });
+        },
+        onError: () => {
+            toast.error("Could not delete the account");
+        }
+    });
+
+    return mutation;
+}
